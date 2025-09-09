428a7cbe2b05300f00c2031ff283d8a369bf2df2
Anahi <anahimilenagimenez@gmail.com>
2025-09-09 18:46:23 -0300

    primera subida

---
+++ TPWinForm_Presentacion/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace TPWinForm_Presentacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvArticulo = new System.Windows.Forms.DataGridView();
+            this.fpImagen = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvArticulo
+            // 
+            this.dgvArticulo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvArticulo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvArticulo.Location = new System.Drawing.Point(23, 64);
+            this.dgvArticulo.Name = "dgvArticulo";
+            this.dgvArticulo.Size = new System.Drawing.Size(500, 258);
+            this.dgvArticulo.TabIndex = 0;
+            this.dgvArticulo.SelectionChanged += new System.EventHandler(this.dgvArticulo_SelectionChanged);
+            // 
+            // fpImagen
+            // 
+            this.fpImagen.AutoScroll = true;
+            this.fpImagen.Location = new System.Drawing.Point(548, 64);
+            this.fpImagen.Name = "fpImagen";
+            this.fpImagen.Size = new System.Drawing.Size(240, 258);
+            this.fpImagen.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.fpImagen);
+            this.Controls.Add(this.dgvArticulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvArticulo;
+        private System.Windows.Forms.FlowLayoutPanel fpImagen;
+    }
+}
+
---
+++ negocio/ArticuloNegocio.cs
@@ -0,0 +1,75 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace negocio
+{
+    public class ArticuloNegocio
+    {
+        public List<Articulo> Listar()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            //connection
+            SqlConnection conexion = new SqlConnection("Server=.\\SQLEXPRESS; Initial Catalog= CATALOGO_P3_DB; Integrated Security=true;");
+            //command
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexion;
+            comando.CommandText = @"
+    SELECT a.Id, a.Codigo, a.Nombre, a.Descripcion,
+           m.Descripcion AS Marca,
+           c.Descripcion AS Categoria,
+           a.Precio
+    FROM ARTICULOS a
+    INNER JOIN MARCAS m ON a.IdMarca = m.Id
+    INNER JOIN CATEGORIAS c ON a.IdCategoria = c.Id";
+            //sql datareader
+            SqlDataReader lector;
+
+            conexion.Open();
+            lector= comando.ExecuteReader();
+
+            while (lector.Read()) 
+            { 
+                Articulo articulo = new Articulo();
+                articulo.Id = (int)lector["Id"];
+                articulo.Codigo = (string)lector["Codigo"];
+                articulo.Nombre = (string)lector["Nombre"];
+                articulo.Descripcion = (string)lector["Descripcion"];
+
+                articulo.Marca = new Marca();
+                articulo.Marca.Descripcion = (string)lector["Marca"];
+
+                articulo.Categoria = new Categoria();
+                articulo.Categoria.Descripcion = (string)lector["Categoria"];
+
+                articulo.Precio = (decimal)lector["Precio"];
+
+                lista.Add(articulo);
+            }
+            conexion.Close();
+
+
+            return lista;
+        }
+        //nuevo articulo
+        public void Agregar(Articulo nuevo)
+        {
+            SqlConnection conexion = new SqlConnection("Server=.\\SQLEXPRESS; Initial Catalog= CATALOGO_P3_DB; Integrated Security=true;");
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexion; 
+            comando.CommandText= "INSERT INTO ARTICULOS (Codigo, Nombre, Descripcion, IdMarca, IdCategoria, Precio) " +
+                          "VALUES ('" + nuevo.Codigo + "', '" + nuevo.Nombre + "', '" + nuevo.Descripcion + "', " +
+                          nuevo.Marca.Id + ", " + nuevo.Categoria.Id + ", " + nuevo.Precio + ")";
+
+            conexion.Open();
+            comando.ExecuteNonQuery();///consulta no devuelve para insert
+            conexion.Close();
+
+        }
+    }
+}
